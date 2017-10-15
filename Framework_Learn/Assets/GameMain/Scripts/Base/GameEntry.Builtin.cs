f3929cf458fd59ba7b6a06ed301aadbb83327bb7
Game-OneD <513877460@qq.com>
2017-10-15 21:25:52 +0800

    初始化

---
+++ Framework_Learn/Assets/GameMain/Scripts/Base/GameEntry.Builtin.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+public partial class GameEntry
+{
+    public static int Base
+    {
+        get;
+        private set;
+    }
+
+    private string DebugContent = string.Empty;
+
+    private void InitBuiltinComponents()
+    {
+        Base = 233;
+        UnityEngine.Debug.LogFormat("Base = {0}", Base);
+        DebugLogTest("Debug Only");
+    }
+
+    private void OnGUI()
+    {
+        UnityEngine.GUILayout.Label("Begin:");
+        UnityEngine.GUILayout.Label(m_DebugContent);
+    }
+
+    private string m_DebugContent = string.Empty;
+
+    [Conditional("DEBUG")]
+    public void DebugLogTest(string content)
+    {
+        m_DebugContent = content;
+    }
+}
